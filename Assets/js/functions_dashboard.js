e3502fe3e700d8587c6b3590ecdf81b642846e33
migueltafur03 <miguelmita04@gmail.com>
2024-09-02 10:11:12 -0300

    terminando de implementar la seccion de manual de usuarios 2

--- Assets/js/functions_dashboard.js
+++
@@ -1,2 +0,0 @@
-let divLoading = document.querySelector("#divLoading");
-
--- Views/Manual/manualEquipamentos.php
+++ Views/Manual/manualEquipamentos.php
@@ -7,5 +7,79 @@
                 </h1>
             </div>
         </div>
+
+        <div class="container-fluid">
+            <div class="tile">
+                <div class="tile-body">
+                    <div class="accordion" id="accordionEquipamentos">
+                        <div class="card">
+                            <div class="card-header" id="headingCriar">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        <i class="fa fa-plus-circle" aria-hidden="true" style="margin-top: -3px;"></i>CRIAR EQUIPAMENTO
+                                    </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingCriar" data-parent="#accordionEquipamentos">
+                                <div class="card-body">
+                                    <video controls class="w-100">
+                                        <!-- <source src="<?= media(); ?>/videos/criarusuario.mp4" type="video/mp4" /> -->
+                                    </video>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingAlterar">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        <i class="fa fa-pencil-alt" aria-hidden="true" style="margin-top: -3px;"></i>ALTERAR EQUIPAMENTO
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingAlterar" data-parent="#accordionEquipamentos">
+                                <div class="card-body">
+                                    <video controls class="w-100">
+                                        <!-- <source src="<?= media(); ?>/videos/alterarusuario.mp4" type="video/mp4" /> -->
+                                    </video>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingRemover">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        <i class="fa fa-trash-alt" aria-hidden="true" style="margin-top: -3px;"></i>REMOVER EQUIPAMENTO
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingRemover" data-parent="#accordionEquipamentos">
+                                <div class="card-body">
+                                    <video controls class="w-100">
+                                        <!-- <source src="<?= media(); ?>/videos/removerusuario.mp4" type="video/mp4" /> -->
+                                    </video>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingGrafico">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                        <i class="fa fa-bar-chart" aria-hidden="true" style="margin-top: -3px;" ></i>GRÁFICO DE EQUIPAMENTO
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseFour" class="collapse" aria-labelledby="headingGrafico" data-parent="#accordionEquipamentos">
+                                <div class="card-body">
+                                    <video controls class="w-100">
+                                        <!-- <source src="<?= media(); ?>/videos/graficousuario.mp4" type="video/mp4" /> -->
+                                    </video>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 <?php footerAdmin($data); ?>    
